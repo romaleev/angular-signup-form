4d867edb9e2a015ec54ca22cd3da533db337803c
romaleev <aromaleev@gmail.com>
2023-05-24 16:04:55 +0800

    add i18next internationalization

---
+++ i18next.config.ts
@@ -0,0 +1,13 @@
+import enLanguage from './src/assets/locales/en.language.json';
+
+export default {
+  supportedLngs: ['en'],
+  fallbackLng: 'en',
+  resources: {
+    en: {
+      translation: enLanguage
+    }
+  },
+  debug: false,
+  returnEmptyString: false
+};
